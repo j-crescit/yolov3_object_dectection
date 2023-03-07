ccd6c0897b0c3de2506c6eeea646dc35df9ff638
sehun <seihun2z@gmail.com>
2023-03-07 13:41:02 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+# define constants
+
+# load class names
+
+# load model
+
+# load image
+
+# convert image
+
+# get detections
+
+# bounding box, class id, confidence
+
+# apply nms
+
+# plot
